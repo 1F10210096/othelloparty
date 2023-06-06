c8055a2482302d901b7c4d0a4b53b3283ce47213
1F10210096 <koki.1218.dog@icloud.com>
2023-06-06 21:30:10 +0900

    othelloparty

--- server/repository/boardRepository.ts
+++ server/repository/boardRepository.ts
@@ -1,6 +1,5 @@
-import type { UserId } from "$/commonTypesWithClient/branded";
-import { userColorRepository } from "./userColorRepositry";
-
+import type { UserId } from '$/commonTypesWithClient/branded';
+import { userColorRepository } from './userColorRepositry';
 
 export type BoardArr = number[][];
 
@@ -15,25 +14,8 @@ const board: BoardArr = [
 
 export const boardRepository = {
   getBoard: () => board,
-  clickBoard: (x: number, y: number,userId:UserId): number[][] => {
+  clickBoard: (x: number, y: number, userId: UserId): number[][] => {
     board[y][x] = userColorRepository.getUserColor(userId);
     return board;
   },
 };
-
-
-curl 'http://localhost:31577/api/me' \
-  -H 'Accept: application/json, text/plain, */*' \
-  -H 'Accept-Language: ja,en-US;q=0.9,en;q=0.8' \
-  -H 'Connection: keep-alive' \
-  -H 'Cookie: _ga=GA1.1.2023351821.1685983241; _gid=GA1.1.1025738352.1685983241; _gat_gtag_UA_SOMENUMBER_X=1; session=eyJhbGciOiJub25lIiwidHlwIjoiSldUIn0.eyJuYW1lIjoiQ2hpY2tlbiBNb3VudGFpbiIsImVtYWlsIjoiY2hpY2tlbi5tb3VudGFpbi41N0BleGFtcGxlLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhdXRoX3RpbWUiOjE2ODU5ODM1ODMsInVzZXJfaWQiOiJzOTRvampHVTZqbFNGdXBKaVVBSUR2SUpQSlpMIiwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJjaGlja2VuLm1vdW50YWluLjU3QGV4YW1wbGUuY29tIl0sImdpdGh1Yi5jb20iOlsiODQyMTA0MjYxMDAxMjMyMDI5MTU5MzY1NzAxNjg1NjU5NDg3NTYwNyJdfSwic2lnbl9pbl9wcm92aWRlciI6ImdpdGh1Yi5jb20ifSwiaWF0IjoxNjg2MDQ3NDM2LCJleHAiOjE2ODY0Nzk0MzYsImF1ZCI6ImVtdWxhdG9yIiwiaXNzIjoiaHR0cHM6Ly9zZXNzaW9uLmZpcmViYXNlLmdvb2dsZS5jb20vZW11bGF0b3IiLCJzdWIiOiJzOTRvampHVTZqbFNGdXBKaVVBSUR2SUpQSlpMIn0.' 
-  -H 'Origin: http://localhost:3000' \
-  -H 'Referer: http://localhost:3000/' \
-  -H 'Sec-Fetch-Dest: empty' \
-  -H 'Sec-Fetch-Mode: cors' \
-  -H 'Sec-Fetch-Site: same-site' \
-  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36' \
-  -H 'sec-ch-ua: "Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"' \
-  -H 'sec-ch-ua-mobile: ?0' \
-  -H 'sec-ch-ua-platform: "Windows"' \
-  --compressed
