6c98d0a0ee6a5398f69009e02005cd6472b316cf
1F10210096 <s1F102100962@iniad.org>
2023-07-06 17:54:08 +0900

    レート追加途中2

---
+++ server/usecase/userPoint.ts
@@ -0,0 +1,12 @@
+import type { UserPointModel } from "$/commonTypesWithClient/models"
+import { userPointRepository } from "$/repository/roomRepository"
+
+export const userPoint = {
+  create: async () => {
+    const newUserPoint: UserPointModel= {
+      id:"a",
+      userpoint:1000
+    }
+    await userPointRepository.save(newUserPoint)
+  }
+}
